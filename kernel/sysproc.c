f11eee4314e17775f5e30da57ad6722c36096326
zhouzilong <529620861@qq.com>
2023-01-07 14:59:43 -0500

    reformat

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -1,65 +1,53 @@
 #include "types.h"
+//
 #include "riscv.h"
+//
 #include "param.h"
+//
 #include "defs.h"
+//
 #include "memlayout.h"
+//
 #include "spinlock.h"
+//
 #include "proc.h"
 
-uint64
-sys_exit(void)
-{
+uint64 sys_exit(void) {
   int n;
   argint(0, &n);
   exit(n);
   return 0;  // not reached
 }
 
-uint64
-sys_getpid(void)
-{
-  return myproc()->pid;
-}
+uint64 sys_getpid(void) { return myproc()->pid; }
 
-uint64
-sys_fork(void)
-{
-  return fork();
-}
+uint64 sys_fork(void) { return fork(); }
 
-uint64
-sys_wait(void)
-{
+uint64 sys_wait(void) {
   uint64 p;
   argaddr(0, &p);
   return wait(p);
 }
 
-uint64
-sys_sbrk(void)
-{
+uint64 sys_sbrk(void) {
   uint64 addr;
   int n;
 
   argint(0, &n);
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  if (growproc(n) < 0) return -1;
   return addr;
 }
 
-uint64
-sys_sleep(void)
-{
+uint64 sys_sleep(void) {
   int n;
   uint ticks0;
-
 
   argint(0, &n);
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
+  while (ticks - ticks0 < n) {
+    if (killed(myproc())) {
       release(&tickslock);
       return -1;
     }
@@ -79,9 +67,7 @@ sys_pgaccess(void)
 }
 #endif
 
-uint64
-sys_kill(void)
-{
+uint64 sys_kill(void) {
   int pid;
 
   argint(0, &pid);
@@ -90,9 +76,7 @@ sys_kill(void)
 
 // return how many clock tick interrupts have occurred
 // since start.
-uint64
-sys_uptime(void)
-{
+uint64 sys_uptime(void) {
   uint xticks;
 
   acquire(&tickslock);
