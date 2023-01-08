82f9c671bd2a31b71b53a4f222341896a4eacf8e
zhouzilong <529620861@qq.com>
2023-01-08 12:40:48 -0500

    format

--- user/call.c
+++ user/call.c
@@ -1,17 +1,16 @@
 #include "kernel/param.h"
+//
 #include "kernel/types.h"
+//
 #include "kernel/stat.h"
+//
 #include "user/user.h"
 
-int g(int x) {
-  return x+3;
-}
+int g(int x) { return x + 3; }
 
-int f(int x) {
-  return g(x);
-}
+int f(int x) { return g(x); }
 
 void main(void) {
-  printf("%d %d\n", f(8)+1, 13);
+  printf("%d %d\n", f(8) + 1, 13);
   exit(0);
 }
